7e43c13aac0c35acfb372c9dad59ce26621ecc89
Tim Rodger <tim.rodger@gmail.com>
2017-02-26 17:03:58 +0000

    moves handler file, adds functions

--- handler.js
+++ handler.js
@@ -1,16 +1,50 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.add = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: 'succcess'
+    })
   };
 
   callback(null, response);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
+module.exports.get = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'succcess'
+    })
+  };
+
+  callback(null, response);
+
+};
+
+module.exports.update = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'succcess'
+    })
+  };
+
+  callback(null, response);
+
+};
+
+
+module.exports.delete = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'succcess'
+    })
+  };
+
+  callback(null, response);
+
 };

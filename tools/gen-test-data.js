dbaa0febd52a85412db4b0e6c4eb34d2e40966da
Tim Rodger <tim.rodger@gmail.com>
2017-03-31 17:29:28 +0100

    Updates load testing script

---
+++ tools/gen-test-data.js
@@ -0,0 +1,14 @@
+const uuid = require('uuid');
+
+const number = process.argv[2];
+
+const names = ['tim', 'frank', 'horatio', 'milly', 'mary', 'davina', 'jon', 'henrietta', 'thomas', 'zoro'];
+const domains = ['gmail.com', 'ms.com', 'dofus.net', 'gov.org', 'amazon.com', 'evil-empire.co.uk'];
+
+console.log('id,email,number');
+
+for(i = 0; i < number; i++) {
+    var num = Math.floor((Math.random() * 100000 ) + 1);
+    var email = names[i % 10] + '-' + Math.floor((Math.random() * 100 )) + '@' + domains[i % 6];
+    console.log(uuid.v4() + ',' + email + ',' + num);
+}
